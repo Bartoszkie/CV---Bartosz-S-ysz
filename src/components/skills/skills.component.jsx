faf58e1b0477d367b8df5efc890a8a4523e1fdb6
Bartoszkie <bartek.slysz23@gmail.com>
2019-10-04 10:53:41 +0200

    simple changes

--- src/components/skills/skills.component.jsx
+++ src/components/skills/skills.component.jsx
@@ -7,8 +7,8 @@ function Skills() {
   const textJS = "So, when it comes to JS I feel very comofrtable with it. All fundamentals about JS are familiar to me. Both ES5 and ES6 features are not scary to me - BTW I love ES6!";
   const textCSS = "I love positioning. I think I know tons of new features of CSS and I feel very familiar with every new paradigms of positioning like FlexBox and CSS Grid - and also those old ones like Floats aren't unfamiliar to me!."
   const textHTML = "Yeah I even know how to program in HTML... of course it is a joke! There are tons of blocks that are known to me. I know how and when to use them, what are advantages and disatvantages of one above the other, and I'm always trying to write web skeleton as simple as it is possible."
-  const textReact = "When it comes to my first superpower, it is React! I love the way of simplicity in React.js. All paradigms of React and how it behaves when it comes to render components are known to me. All extra features/packages like Redux, ContextAPI, styled-components, HOC, routing, Redux-Saga, React Hooks...I just love it and freedom of choosing tools when it comes to build/solve interfaces/problems."
-  const textSASS = "As my second superpower I would say something about Sass. The way that Sass simplify things in CSS is just asthonishing. All it's features like mixins, variables, imports, nesting classes are just tools that I can't imagine living without right now."
+  const textReact = "When it comes to my first superpower, it is React! I love the way of simplicity in React.js. All paradigms of React and how it behaves when it comes to render components are known to me. All extra features/packages like Redux, ContextAPI, styled-components, HOC patterns, routing, Redux-Saga, React Hooks...I just love it and freedom of choosing tools when it comes to build/solve interfaces/problems."
+  const textSASS = "As my second superpower I would say something about Scss. The way that Scss simplify things in CSS is just asthonishing. All it's features like mixins, variables, imports, nesting classes are just tools that I can't even imagine living without right now."
   const textJava = "And there she is. As big and old as my lovely grandma. But for me - It isn't scary. I would even risk to say that I love it! OOP paradigm is just beautifull for me (I know all of its phillars - encapsulation, abstraction, extending, polymorphism). JavaFX is something that I use in my University but personally my usage of Java is in Spring - to create backend side of web apps. "
 
   return (
@@ -19,7 +19,7 @@ function Skills() {
         <SkillCart skill={'CSS 3'} heading={'CSS'} text={textCSS}/>
         <SkillCart skill={'HTML 5'} heading={'HTML 5'} text={textHTML}/>
         <SkillCart skill={'React.js'} heading={'React.js'} text={textReact}/>
-        <SkillCart skill={'SASS'} heading={'SASS'} text={textSASS}/>
+        <SkillCart skill={'SCSS'} heading={'SCSS'} text={textSASS}/>
         <SkillCart skill={'JAVA'} heading={'JAVA'} text={textJava}/>
         </div>
       <Button text={'See bruh'}/>
