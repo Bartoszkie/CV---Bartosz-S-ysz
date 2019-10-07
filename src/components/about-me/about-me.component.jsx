73f1daad05c8b39a949d75b5a0400fbc115e365a
Bartoszkie <bartosz.slysz23@gmail.com>
2019-10-07 16:18:13 +0200

    added all of missing features

--- src/components/about-me/about-me.component.jsx
+++ src/components/about-me/about-me.component.jsx
@@ -11,33 +11,50 @@ const AboutMe = () => {
         <div className="aboutMe-content__paragraphs">
           <div className="aboutMe-content__paragraphs--edu">
             <p className="paragraph">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              Right, little bit about me. So just like in header - I am 21 and
+              my name is Bartosz Slysz. I've graduated with honors technical
+              Academic High School developed by WSIiZ in Rzeszow. Right now I am
+              3rd year student of IT at University of Rzeszow. Also I am active
+              participant of two science clubs on our University - Science Club
+              of Informatics and Mechatron (Science Club of Mechatronics. Role -
+              programmer). In programming mostly I am self learner. I like
+              'complexity' and figuring out a way how to solve this complexity.
+              When it comes to say about the way which I have choosen for
+              programming, right now at the moment I am trying to become
+              proffesional FrontEnd Dev. I have fall in love in modern
+              JavaScript and ways of creating astonishing user interfaces and
+              apps. Plans for future when it comes about carrier? Become full
+              stack dev and be able to create breathtaking web apps and static
+              mobile apps.
             </p>
           </div>
 
           <div className="aboutMe-content__paragraphs--exp">
-            <p className="paragraph">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+            <ul className="aboutMe-content__paragraphs--exp--list">
+              <li className="exp-item">
+                <p className="paragraph">
+                  2015 - MobiTouch: Graphics and WebDev
+                </p>
+              </li>
+              <li className="exp-item">
+                <p className="paragraph">
+                  2018 - PGS Software WorkShops: Graduated with honours JAVA
+                  WorkShops.
+                </p>
+              </li>
+              <li className="exp-item">
+                <p className="paragraph">
+                  2019 - IDEO: Internship as FrontEnd Developer - Mainly: Coding
+                  layouts and creating simple web apps in React with usage of
+                  Redux
+                </p>
+              </li>
+              <li className="exp-item">
+                <p className="paragraph">
+                  2018 - untill now: Creating simple freelancing tasks.
+                </p>
+              </li>
+            </ul>
           </div>
         </div>
 
