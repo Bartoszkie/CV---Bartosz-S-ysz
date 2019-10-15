72cfa40ee18bacd7204f9810c6fe480c2e51a611
Bartoszkie <bartosz.slysz23@gmail.com>
2019-10-15 11:02:01 +0200

    added build

---
+++ build/precache-manifest.4323bb0e1c20cd3cdc574b4ff46825b0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3d38d1981c040a71ae53e6509634db",
+    "url": "/CV---Bartosz-S-ysz/index.html"
+  },
+  {
+    "revision": "2fc7c32e0b2a989a5607",
+    "url": "/CV---Bartosz-S-ysz/static/css/main.edf0015a.chunk.css"
+  },
+  {
+    "revision": "be294cc9e70a1f4253eb",
+    "url": "/CV---Bartosz-S-ysz/static/js/2.c91990b7.chunk.js"
+  },
+  {
+    "revision": "2fc7c32e0b2a989a5607",
+    "url": "/CV---Bartosz-S-ysz/static/js/main.bf4109b2.chunk.js"
+  },
+  {
+    "revision": "0927c7464a55eb97c5c1",
+    "url": "/CV---Bartosz-S-ysz/static/js/runtime-main.67db4d13.js"
+  },
+  {
+    "revision": "d0053a790b23288140b0e1f1cbee45d3",
+    "url": "/CV---Bartosz-S-ysz/static/media/Ja.d0053a79.jpg"
+  },
+  {
+    "revision": "9b15b70d1188b979507e77343d990479",
+    "url": "/CV---Bartosz-S-ysz/static/media/Natoure-mobile.9b15b70d.png"
+  },
+  {
+    "revision": "268b398b2a3ce3b09303617004f7cbd0",
+    "url": "/CV---Bartosz-S-ysz/static/media/Natoure.268b398b.png"
+  },
+  {
+    "revision": "0e9d0d8954b2b724b5852fa75fd6ba0d",
+    "url": "/CV---Bartosz-S-ysz/static/media/Nexter-mobile.0e9d0d89.png"
+  },
+  {
+    "revision": "77245d56b6c264857fc81db2b349a14b",
+    "url": "/CV---Bartosz-S-ysz/static/media/Nexter.77245d56.png"
+  },
+  {
+    "revision": "4e0bd36efd5bc6f435b640846e5b54e9",
+    "url": "/CV---Bartosz-S-ysz/static/media/Shop.4e0bd36e.png"
+  },
+  {
+    "revision": "461f77429aa545ce15c931e470f4139f",
+    "url": "/CV---Bartosz-S-ysz/static/media/Trillo-mobile.461f7742.png"
+  },
+  {
+    "revision": "d9fbd77bb2f0c397060cc0667ec48701",
+    "url": "/CV---Bartosz-S-ysz/static/media/Trollo.d9fbd77b.png"
+  },
+  {
+    "revision": "b1d1fae6da121ccb5d5c662dcad8da5a",
+    "url": "/CV---Bartosz-S-ysz/static/media/css.b1d1fae6.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/CV---Bartosz-S-ysz/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "2c224f8151107740bd0ca61109862f4c",
+    "url": "/CV---Bartosz-S-ysz/static/media/java.2c224f81.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/CV---Bartosz-S-ysz/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/CV---Bartosz-S-ysz/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/CV---Bartosz-S-ysz/static/media/sass.b446ca5a.svg"
+  }
+]);
